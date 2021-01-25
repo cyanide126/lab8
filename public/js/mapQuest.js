fc7fc62af67d86408991a4f2dbb2ed9c64317bd5
cyanide126 <cyada2000@gmail.com>
2021-01-25 13:46:44 +0700

    added

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'K3ids53h5il7l7yaAmOyMKrrrewHy2yL';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	    center: [32.88, -117.236],
+	    layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
